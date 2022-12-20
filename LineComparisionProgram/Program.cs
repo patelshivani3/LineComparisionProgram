0ad929af2cbdcb77a1d76d3b20b277f0da450e66
patelshivani3 <shivpatel3001@gmail.com>
2022-12-20 11:36:39 +0530

    Adding Equality of Lines

--- LineComparisionProgram/Program.cs
+++ LineComparisionProgram/Program.cs
@@ -20,12 +20,35 @@ namespace LineComparisionProgram
             Console.WriteLine("Please Enter The X2 & Y2 Co-Ordinates:");
             var X2 = Convert.ToDouble(Console.ReadLine());
             var Y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please Enter X3 & Y3 Co-Ordinates:");
+            var X3 = Convert.ToDouble(Console.ReadLine());
+            var Y3 = Convert.ToDouble(Console.ReadLine());
             
+            Console.WriteLine("Please Enter X4 & Y4 Co-Ordinates:");
+            var X4 = Convert.ToDouble(Console.ReadLine());
+            var Y4 = Convert.ToDouble(Console.ReadLine());
+           
             double X = Math.Pow((X2 - X1), 2);
             double Y = Math.Pow((Y2 - Y1), 2);
+            double firstLineLength = Math.Sqrt(X + Y);
+            Console.WriteLine("Length Of First Line is :" + firstLineLength);
             
-            double lineLength = Math.Sqrt(X + Y);
-            Console.WriteLine("Length Of Line is :" + lineLength);
+            
+            
+            double x = Math.Pow((X4 - X3), 2);
+            double y = Math.Pow((Y4 - Y3), 2);
+            double secondLineLength = Math.Sqrt(x + y);
+            Console.WriteLine("Length Of Second Line is :" + secondLineLength);
+            
+            if (firstLineLength == secondLineLength)
+            {
+                Console.WriteLine("Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not Equal");
+            }
             Console.ReadLine();
         }
         

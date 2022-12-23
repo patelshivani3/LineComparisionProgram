a406623854e57e73d797b7d208d695b2276b22ac
patelshivani3 <shivpatel3001@gmail.com>
2022-12-23 21:01:14 +0530

    Updating The Program Using CompareToUC4

--- LineComparisionProgram/Program.cs
+++ LineComparisionProgram/Program.cs
@@ -11,12 +11,16 @@ namespace LineComparisionProgram
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to line comparision program ");
-           
+            LineComparision();
+            Console.ReadLine();
+        }
+        public static void LineComparision()
+        {
             //UC1 Length of the line
             Console.WriteLine("Please Enter The X1 & Y1 Co-Ordinates:");
             var X1 = Convert.ToDouble(Console.ReadLine());
             var Y1 = Convert.ToDouble(Console.ReadLine());
-           
+
             Console.WriteLine("Please Enter The X2 & Y2 Co-Ordinates:");
             var X2 = Convert.ToDouble(Console.ReadLine());
             var Y2 = Convert.ToDouble(Console.ReadLine());
@@ -24,36 +28,32 @@ namespace LineComparisionProgram
             Console.WriteLine("Please Enter X3 & Y3 Co-Ordinates:");
             var X3 = Convert.ToDouble(Console.ReadLine());
             var Y3 = Convert.ToDouble(Console.ReadLine());
-            
+
             Console.WriteLine("Please Enter X4 & Y4 Co-Ordinates:");
             var X4 = Convert.ToDouble(Console.ReadLine());
             var Y4 = Convert.ToDouble(Console.ReadLine());
-           
-            double X = Math.Pow((X2 - X1), 2);
-            double Y = Math.Pow((Y2 - Y1), 2);
-            double firstLineLength = Math.Sqrt(X + Y);
+
+            double firstLineLength = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
             Console.WriteLine("Length Of First Line is :" + firstLineLength);
-            
-            double x = Math.Pow((X4 - X3), 2);
-            double y = Math.Pow((Y4 - Y3), 2);
-            double secondLineLength = Math.Sqrt(x + y);
+
+            double secondLineLength = Math.Sqrt(Math.Pow((X4 - X3), 2) + Math.Pow((Y4 - Y3), 2));
             Console.WriteLine("Length Of Second Line is :" + secondLineLength);
+
+            int compare = firstLineLength.CompareTo(secondLineLength);
+            Console.WriteLine("Comparision is :"+compare);
             
-            if (firstLineLength == secondLineLength)
+            if (compare == 0)
             {
                 Console.WriteLine("Lines are Equal");
             }
-            else if(firstLineLength > secondLineLength)
+            else if (compare > 0)
             {
                 Console.WriteLine("First line is Greater than Second line");
             }
-            else if (firstLineLength < secondLineLength)
+            else 
             {
                 Console.WriteLine("First line is Smaller than Second line");
             }
-            Console.ReadLine();
         }
-        
-
     }
 }
